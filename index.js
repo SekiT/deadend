f3073d5cbf57ec5bdaaa9fad7f4fe28517647d71
Takaaki Seki <takaaki.seki@access-company.com>
2018-08-09 00:31:27 +0900

    Example should use path parameter

--- index.js
+++ index.js
@@ -2,9 +2,10 @@ import Router from './core/Router';
 import Route from './core/Route';
 
 const router = new Router(window, '/', [
-  new Route('/', (params) => { console.log(params); }, [
-    new Route('foo', (params) => { console.log(params); }),
+  new Route('/', () => {}, [
+    new Route('foo', () => {}),
+    new Route('bar/:barName', ({ params }) => { console.log(params); }),
   ]),
 ]);
 
-router.moveTo('/foo');
+router.moveTo('/bar/baz');
