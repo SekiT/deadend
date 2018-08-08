b67983f93164435688ecb5dae328d53dfabceabd
Takaaki Seki <takaaki.seki@access-company.com>
2018-08-08 18:30:46 +0900

    class Route

---
+++ core/Route.js
@@ -0,0 +1,25 @@
+export class Route {
+  constructor(pathTemplate, handler, children) {
+    const regexSource =
+      pathTemplate
+        .split('/')
+        .slice(1)
+        .map(encodeURI)
+        .map(fragment => fragment.startsWith(':') ? `(?<${fragment.slice(1)}>[^/]+)` : fragment)
+        .join('/');
+    this.pathRegex = new RegExp(`^/${regexSource}$`);
+    this.handler   = handler;
+    this.children  = children;
+  }
+
+  match(path) {
+    const result = this.pathRegex.exec(path);
+    if (result && result.groups) {
+      return {
+        path: result[0],
+        params: result.groups,
+      };
+    }
+    return null;
+  }
+}
