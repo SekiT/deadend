a8acaf8052dcbd2ce84f9e0e21d67ea5c40a08e4
Takaaki Seki <takaaki.seki@access-company.com>
2018-08-09 22:25:41 +0900

    Use rollup-plugin-terser only for dev

--- rollup.config.js
+++ rollup.config.js
@@ -1,5 +1,8 @@
+import { terser } from 'rollup-plugin-terser';
+
 export default {
   input: 'index.js',
+  plugins: process.env.MINIFY ? [terser()] : [],
   output: {
     file: 'bundle.js',
     format: 'iife',
