6526a71c3eb300d4f52f2ab564f49671fd5f9a91
Takaaki Seki <takaaki.seki@access-company.com>
2018-08-18 23:28:34 +0900

    Test Route.match

--- test/RouteTest.js
+++ test/RouteTest.js
@@ -19,4 +19,23 @@ export default () => {
     });
     t.end();
   });
+
+  test('Route matches or not correctly', (t) => {
+    [
+      ['/', '/', { path: '/', params: {} }],
+      ['/', '/path', { path: '/', params: {} }],
+      ['/', '', null],
+      ['/', 'path', null],
+      ['/path', '/path', { path: '/path', params: {} }],
+      ['/path1', '/path1/path2', { path: '/path1', params: {} }],
+      ['/path1', '/path2/path1', null],
+      ['/:param', '/value', { path: '/value', params: { param: 'value' } }],
+      ['/:param1/:param2', '/value1/value2/path', { path: '/value1/value2', params: { param1: 'value1', param2: 'value2' } }],
+      ['/path/:param', '/path/value/subpath', { path: '/path/value', params: { param: 'value' } }],
+    ].forEach(([template, path, expected]) => {
+      const route = new Route(template, () => {}, []);
+      t.deepEqual(route.match(path), expected);
+    });
+    t.end();
+  });
 };
