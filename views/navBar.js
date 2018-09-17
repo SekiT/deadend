ab072c5a5795be9fb19aea10ec14b4059c7180a3
Takaaki Seki <takaaki.seki@access-company.com>
2018-09-17 21:53:14 +0900

    Items for navbar

--- views/navBar.js
+++ views/navBar.js
@@ -1,10 +1,17 @@
 import view, { update } from '../lib/view';
 
-export default view('deadend-navbar', [''], path => ({
+const navItem = (newPath, path, text) => `
+  <a href="#/${path}" class="navbar-item${newPath === path ? ' active' : ''}">${text}</a>
+`;
+
+export default view('deadend-navbar', [''], newPath => ({
   html: `
     <nav>
       <a class="title" href="#">deadend</a>
-      path: ${path}
+      ${navItem(newPath, 'input', 'Using input')}
+      ${navItem(newPath, 'http', 'Using HTTP')}
+      ${navItem(newPath, 'tick', 'Using tick event')}
+      <a class="navbar-item" href="http://github.com/SekiT/deadend">GitHub</a>
     </nav>
   `,
   attachments: {
