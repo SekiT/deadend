1a182374db21df5824e563a90728c428f6253bc4
Takaaki Seki <takaaki.seki@access-company.com>
2018-09-18 11:44:18 +0900

    Temporary commit

--- views/input.js
+++ views/input.js
@@ -1,6 +1,16 @@
 import view from '../lib/view';
 
 export default view('deadend-input', [], () => ({
-  html: 'This is input view',
-  attachments: {},
+  html: `
+    <h1>Using input</h1>
+    <p>
+      This view contains &lt;input&gt;
+    </p>
+    <input type="text"><button>Click</button>
+  `,
+  attachments: {
+    input: {
+      oninput: ({ target }) => console.log(target.value),
+    },
+  },
 }));
