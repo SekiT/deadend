e87525eef322e82f4465519db73929b08ee80851
Takaaki Seki <takaaki.seki@access-company.com>
2018-09-18 16:57:20 +0900

    Tick every second

--- views/tick.js
+++ views/tick.js
@@ -1,6 +1,15 @@
 import view from '../lib/view';
+import tickSubject from '../subjects/tickSubject';
+import tickEverySecond from './tick/tickEverySecond';
 
 export default view('deadend-tick', [], () => ({
-  html: 'This is tick view',
-  attachments: {},
+  html: `
+    <h1>Using tick event</h1>
+    <${tickEverySecond}></${tickEverySecond}>
+  `,
+  attachments: {
+    h1: {
+      none: tickSubject.init(0),
+    },
+  },
 }));
